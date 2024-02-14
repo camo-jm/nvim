28b5b0f342d8d77e70ca1f4058a961565a876efa
ookami <127811291+camo-jm@users.noreply.github.com>
2024-02-14 17:27:44 +0000

    Update init.lua
    - reorganized certain options
    - added the Functions section
    - updated netrw

--- init.lua
+++ init.lua
@@ -1,44 +1,59 @@
 -- https://github.com/camo-jm/nvim
 -- Made by ookami (camo-jm)
 -- welcome to my config, here you have an index of the different sections:
--- Foundation, Status Line, Remaps, Programming, Options
+-- Foundation, Remaps, Programming, Options, Functions
+
 
 -- Foundation
-print(" [ Welcome ]")						-- welcome message
+print(" [ Welcome ]")							-- welcome message
 vim.g.mapleader = " "							-- set leader key
 vim.cmd('set path+=**')							-- set current path
 vim.opt.mouse = 'a'								-- allow mouse
 vim.opt.clipboard = 'unnamedplus'				-- use system clipboard
-vim.o.statusline = " %F %m  %=  [%l;%c]"		-- Left %= Right
+vim.o.statusline = "| %F %m  %=  [%l;%c] |"		-- Left %= Right
+
 
 -- Autocmds
 
---Netrw
+
+-- Editor
+-- organize opts here
+
+
+-- Netrw (vim tree)
+-- to do: fix window tiling when opening a file with 'nvim .', it opens below xd
+vim.keymap.set("n", "<leader>t", ":Lex\n")															-- open netrw, tree style, 25%
+
+vim.o.autochdir = false
+
 vim.g.netrw_winsize = 20
-vim.g.netrw_banner = 0
+vim.g.netrw_banner = 1
 vim.g.netrw_sort_sequence = [[[\/]$,*]]
 vim.g.netrw_sizestyle = "H"
 vim.g.netrw_hide = 1																				-- 0 = all, 1 = not-hidden, 2 = only hidden
+vim.g.netrw_keepdir = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_preview = 0		-- if it is set to 1 it opens to the right, quite weird
 
 -- Remaps
 --vim.keymap.set("", "", "")
-vim.keymap.set("n", "<leader>t", ":Lex\n")															-- open netrw, tree style, 25%
 vim.keymap.set("i", "'", "''<left>")																-- autocomplete ''
 vim.keymap.set("i", "\"", "\"\"<left>")																-- autocomplete ""
 vim.keymap.set("i", "[", "[]<left>")																-- autocomplete []
 vim.keymap.set("i", "{", "{}<left>")																-- autocomplete {}
 vim.keymap.set("i", "<", "<><left>")																-- autocomplete <>
 vim.keymap.set("i", "(", "()<left>")																-- autocomplete ()
---vim.keymap.set("i", "<C-j><C-k>", "<ESC>")														-- ez esc
+vim.keymap.set("i", "<C-j><C-k>", "<ESC>")														-- ez esc
 --vim.keymap.set("n", "um", ":unmap ")																-- ":unmap "
+
 
 -- Programming
 	--C
 vim.keymap.set("n", "<leader>ci", "i#include <stdio.h>\nint main()\n{\n\n}<up>\t")					-- insert c boilerplate
 vim.keymap.set("n", "<leader>cc", ":!make %<<cr>")													-- compile the program
 
-	--Python
-		--to do(?)
+	--Python - to do(?)
 
 -- Packer
 	-- to do (?)
@@ -46,7 +61,8 @@ vim.keymap.set("n", "<leader>cc", ":!make %<<cr>")													-- compile the pr
 -- LSP
 -- to do
 
--- Options (note to self: .opt can handle global & local options, .o can only do global)
+
+-- Options (note to self: .opt can handle global & local options, .o can only do global, need to sort these)
 -- vim.o.=
 vim.o.title = true
 vim.o.number = true
@@ -66,3 +82,4 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.scrolloff = 10
 vim.o.sidescrolloff = 10
+
