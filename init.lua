b4a73986303c54c89a7d69663b4c31632a310fc3
ookami <127811291+camo-jm@users.noreply.github.com>
2024-12-13 06:11:56 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,26 +1,19 @@
 --my minimal NVIM config (camo-jm/nvim). feel free to modify it <3
-
+--
 vim.g.mapleader = " "
 vim.cmd('colorscheme slate')
-vim.g.cb   = 'unnamedplus'
-vim.o.stl  = "| %F %m  %=  [%H|%R] [%l;%c] [%p%%] |"
-vim.o.nu   = true
-
-vim.o.so   = 10
+vim.o.stl = "| %F %m %= [%H|%R] [%l;%c] [%p%%] |"
+vim.o.nu = true
+vim.o.cul = true
+--
+vim.o.so = 10
 vim.o.siso = 10
-vim.o.hls  = false
-vim.o.ic   = true
-vim.o.scs  = true
-
-vim.o.rnu  = true
-vim.o.spr  = true
-vim.o.sb   = true
-vim.o.wrap = false
-vim.o.tgc  = true
-
+vim.o.scs = true
+vim.o.spr = true
+vim.o.sb = true
+--
 vim.keymap.set("n", "<leader>t", ":25Lex\n")
-vim.g.netrw_sizestyle = "H"
-vim.g.netrw_hide = 0
+vim.h.netrw_hide = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
-
+vim.g.netrw_sizestyle = "H"
